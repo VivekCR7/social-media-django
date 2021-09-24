b05d7ae5833635f7fb869155a1ceef9601f77941
Vivek Dubey <vivek.dubey.17@mountblue.tech>
2021-09-24 16:41:09 +0530

    added enviornment variable for secret key

--- social_media_django/settings.py
+++ social_media_django/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a5_xo^4yf(98bhe*9wgrhazvi-snw3ouhp)j^^^n!6i!+%q1^g'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vivek-photogram.herokuapp.com']
 
 
 # Application definition
