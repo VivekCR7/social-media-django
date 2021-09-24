4d743299fc5252e3cbb475506da513b7c23f39ca
Vivek Dubey <vivek.dubey.17@mountblue.tech>
2021-09-24 13:27:40 +0530

    fixed the signal bugs

--- user/models.py
+++ user/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from PIL import Image
 
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpeg', upload_to = 'profile_pics')
+    image = models.ImageField(default='default.png', upload_to = 'profile_pics')
     biography = models.TextField()
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        print("Profile created!")
 
-        img = Image.open(self.image.path)
+post_save.connect(create_profile, sender=User)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+def save_profile(sender, instance, created, **kwargs):
+    if created == False:
+        instance.profile.save()
+        print("Profile updated!")
+
+post_save.connect(save_profile, sender=User)
--- user/views.py
+++ user/views.py
@@ -18,8 +18,8 @@ def register(request):
 
 @login_required
 def profile(request):
-    if request.method == "POST":
-        u_form = UserUpdateForm(request.POST, instance=request.user)
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
                                     request.FILES,
                                     instance=request.user.profile)
@@ -27,15 +27,15 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated!')
+            messages.success(request, f'Your account has been Updated')
             return redirect('profile')
-
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form':u_form,
-        'p_form':p_form,
+        'p_form':p_form
     }
+
     return render(request, 'user/profile.html',context)
