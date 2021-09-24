188355c228ab5e20d5b8a0f9e78f52f8d815ba7f
Vivek Dubey <vivek.dubey.17@mountblue.tech>
2021-09-24 16:10:04 +0530

    postgres database added

--- social_media_django/settings.py
+++ social_media_django/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'social_media_django.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'social_media',
+       'USER': 'vivekcr7',
+       'PASSWORD': 'ronaldo',
+       'HOST': '',
+       'PORT': '',
+   }
 }
 
 
