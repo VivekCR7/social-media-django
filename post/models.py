5a60b6c658053d375712a08dd493cbfd02470da4
Vivek Dubey <vivek.dubey.17@mountblue.tech>
2021-09-25 15:40:42 +0530

    added comments models and also added like attribute in Post model

--- post/models.py
+++ post/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from PIL import Image
 from django.utils import timezone
 from django.urls import reverse
+from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here.
 class Post(models.Model):
@@ -10,6 +11,10 @@ class Post(models.Model):
     image = models.ImageField(upload_to = 'post_pics')
     caption = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+    like = models.ManyToManyField(User,related_name='post_like')
+
+    def total_likes(self):
+        return self.like.count()
 
     def __str__(self):
         return f'{self.author}'
@@ -26,3 +31,17 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail',kwargs={'pk':self.pk})
+        
+class Comment(MPTTModel):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE,
+                            null=True,blank=True)
+    content = models.TextField()
+    date_published = models.DateTimeField(default=timezone.now)
+
+    class MPTTmeta:
+        order_insertion_by = ['date_published']
+
+    def __str__(self):
+        return f'Comment by {self.author}'
