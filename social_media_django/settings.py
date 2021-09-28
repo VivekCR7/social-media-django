17cf7380d95c3779f8478dd2375a5769d09bfee2
Vivek Dubey <vivek.dubey.17@mountblue.tech>
2021-09-28 18:15:51 +0530

    integrated the AWS bucket

--- social_media_django/settings.py
+++ social_media_django/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECRET_KEY = "bb776b6d2f96f4cc60a7196cc808677b8b1faf9d3ced51fd"
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "bb776b6d2f96f4cc60a7196cc808677b8b1faf9d3ced51fd"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
@@ -37,13 +37,13 @@ INSTALLED_APPS = [
     'user',
     'post',
     'crispy_forms',
-    'mptt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -138,5 +138,26 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'post-home'
 LOGIN_URL = 'login'
+
+# configure email setup
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = ""
+# EMAIL_HOST_PASSWORD = ""
+
+
+# setup aws s3 bucket
+AWS_ACCESS_KEY_ID = "AKIAWZJFKQFUDLCFVE7R"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_STORAGE_BUCKET_NAME = "vivek-photogram-media"
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
