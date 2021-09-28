76b60377e5647c6a0d57ef0e6885c59cde826e06
Vivek Dubey <vivek.dubey.17@mountblue.tech>
2021-09-28 10:15:05 +0530

    comments model updated

--- post/models.py
+++ post/models.py
@@ -3,15 +3,16 @@ from django.contrib.auth.models import User
 from PIL import Image
 from django.utils import timezone
 from django.urls import reverse
-from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here.
+
+
 class Post(models.Model):
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
-    image = models.ImageField(upload_to = 'post_pics')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post_pics')
     caption = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    like = models.ManyToManyField(User,related_name='post_like')
+    like = models.ManyToManyField(User, related_name='post_like')
 
     def total_likes(self):
         return self.like.count()
@@ -25,23 +26,32 @@ class Post(models.Model):
         img = Image.open(self.image.path)
 
         if img.height > 600 or img.width > 600:
-            output_size = (600,600)
+            output_size = (600, 600)
             img.thumbnail(output_size)
             img.save(self.image.path)
 
     def get_absolute_url(self):
-        return reverse('post-detail',kwargs={'pk':self.pk})
-        
-class Comment(MPTTModel):
-    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+    def get_comments(self):
+        return self.comments.filter(parent=None).filter(active=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    parent = TreeForeignKey('self', on_delete=models.CASCADE,
-                            null=True,blank=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE,
+                               null=True, blank=True)
     content = models.TextField()
     date_published = models.DateTimeField(default=timezone.now)
+    active = models.BooleanField(default=True)
 
-    class MPTTmeta:
-        order_insertion_by = ['date_published']
+    class Meta:
+        ordering = ('date_published',)
 
     def __str__(self):
         return f'Comment by {self.author}'
+
+    def get_comments(self):
+        return Comment.objects.filter(parent=self).filter(active=True)
