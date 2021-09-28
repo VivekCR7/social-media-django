ea665fa81448ecea2f8de1fba155b0bcafa5dec5
Vivek Dubey <vivek.dubey.17@mountblue.tech>
2021-09-28 10:18:22 +0530

    successfully route and wrote the views for threaded comments

--- post/urls.py
+++ post/urls.py
@@ -9,10 +9,11 @@ from .views import (
 from . import views
 
 urlpatterns = [
-    path('',PostListView.as_view(), name='post-home'),
-    path('post/<int:pk>/',PostDetailView.as_view(), name='post-detail'),
-    path('post/new/',PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update/',PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/',PostDeleteView.as_view(), name='post-delete'),
-    path('post/<int:pk>/like/',views.Like, name='post-like'),  
+    path('', PostListView.as_view(), name='post-home'),
+    path('post/<int:pk>/', views.PostDetailView, name='post-detail'),
+    path('comment/reply/', views.reply_page, name='reply'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/like/', views.Like, name='post-like'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,3 +1,4 @@
+from post.forms import CommentForm
 from django.shortcuts import (
     redirect,
     render,
@@ -6,10 +7,9 @@ from django.shortcuts import (
 )
 from django.urls.base import reverse
 from django.contrib.auth.decorators import login_required
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import (
     ListView,
-    DetailView,
     CreateView,
     UpdateView,
     DeleteView,
@@ -17,75 +17,121 @@ from django.views.generic import (
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
+
 def home(request):
     context = {
-        'posts':Post.objects.all()
+        'posts': Post.objects.all()
     }
-    return render(request,'post/home.html',context)
+    return render(request, 'post/home.html', context)
+
 
 class PostListView(ListView):
     model = Post
     template_name = 'post/home.html'
     ordering = ['-date_posted']
     context_object_name = 'posts'
-    
-class PostDetailView(DetailView):
-    model = Post
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(PostDetailView, self).get_context_data(**kwargs)
 
-        stuff = get_object_or_404(Post,id = self.kwargs['pk'])
-        total = stuff.total_likes()
+@login_required
+def PostDetailView(request, pk):
+    post = get_object_or_404(Post, id=pk)
 
-        liked = False
-        if stuff.like.filter(id = self.request.user.id).exists():
-            liked = True
+    # get_the_likes
+    total = post.total_likes()
+    liked = False
+    if post.like.filter(id=request.user.id).exists():
+        liked = True
 
-        context['total_likes'] = total
-        context['liked']  = liked
+    # list of active comments
+    comments = post.comments.filter(active=True)
+    new_comment = None
 
-        return context
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.author = request.user
+            new_comment.save()
+
+            return redirect(post.get_absolute_url() + "#" + str(new_comment.id))
+    else:
+        comment_form = CommentForm()
+
+    context = {
+        'post': post,
+        'comments': comments,
+        'comment_form': comment_form,
+        'total_likes': total,
+        'liked': liked,
+    }
+    return render(request, 'post/post_detail.html', context)
+
+# handle reply
+
+
+def reply_page(request):
+    if request.method == "POST":
+
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post_id = request.POST.get('post_id')
+            parent_id = request.POST.get('parent')
+            post_url = request.POST.get('post_url')
+
+            reply = form.save(commit=False)
+            reply.author = request.user
+            reply.post = Post(id=post_id)
+            reply.parent = Comment(id=parent_id)
+            reply.save()
+
+            return redirect(post_url+"#"+str(reply.id))
+
+    return redirect("/")
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['image','caption']
+    fields = ['image', 'caption']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['image','caption']
+    fields = ['image', 'caption']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-    #check for the current user who is updating the post
+    # check for the current user who is updating the post
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
             return True
         return False
+
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
 
-    #check for the current user who is deleting the post
+    # check for the current user who is deleting the post
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
             return True
         return False
 
+
 @login_required
 def Like(request, pk):
-    post = get_object_or_404(Post,id = request.POST.get('post_id'))
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
     liked = False
-    if post.like.filter(id = request.user.id).exists():
+    if post.like.filter(id=request.user.id).exists():
         post.like.remove(request.user)
         liked = False
     else:
