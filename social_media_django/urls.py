c34f3427fcb2369c46679a747e3a390d96f14b64
Vivek Dubey <vivek.dubey.17@mountblue.tech>
2021-09-29 16:06:35 +0530

    added routes for changing passwords

--- social_media_django/urls.py
+++ social_media_django/urls.py
@@ -39,6 +39,11 @@ urlpatterns = [
     auth_views.PasswordResetConfirmView.as_view(
         template_name = 'user/password_reset_confirm.html'),name = "password_reset_confirm"
     ),
+    path('password-reset-complete/',
+    auth_views.PasswordResetCompleteView.as_view(
+        template_name = 'user/password_reset_complete.html'
+    ),
+    name = "password_reset_complete"),
     path('',include('post.urls')),
 ] 
 
