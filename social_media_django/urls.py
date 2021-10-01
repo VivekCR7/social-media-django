445ce94902fc0d29ff69cbbb38a6349ba9d083b0
Vivek Dubey <vivek.dubey.17@mountblue.tech>
2021-10-01 11:33:57 +0530

    Added email validation form in password reset route

--- social_media_django/urls.py
+++ social_media_django/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from user.forms import EmailValidationForm
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from user import views as user_views
@@ -28,16 +29,19 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name = 'user/logout.html'),name = "logout"),
     path('password-reset/',
     auth_views.PasswordResetView.as_view(
-        template_name = 'user/password_reset.html'
+        template_name = 'user/password_reset.html',
+        form_class=EmailValidationForm
     ),
     name = "password_reset"),
     path('password-reset/done/',
     auth_views.PasswordResetDoneView.as_view(
-        template_name = 'user/password_reset_done.html'),name = "password_reset_done"
+        template_name = 'user/password_reset_done.html'),
+        name = "password_reset_done"
     ),
     path('password-reset-confirm/<uidb64>/<token>/',
     auth_views.PasswordResetConfirmView.as_view(
-        template_name = 'user/password_reset_confirm.html'),name = "password_reset_confirm"
+        template_name = 'user/password_reset_confirm.html'),
+        name = "password_reset_confirm"
     ),
     path('password-reset-complete/',
     auth_views.PasswordResetCompleteView.as_view(
